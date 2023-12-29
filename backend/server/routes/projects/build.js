69b84956b20d48e7f9e557e4ecdd9088c794c8df
abdulrhman.ali@bld.ai <abdulrhman.ali@bld.ai>
2023-12-29 21:40:43 +0300

    create base build api

--- backend/server/routes/projects/build.js
+++ backend/server/routes/projects/build.js
@@ -4,10 +4,84 @@ import {
   StatusCodes
 } from 'http-status-codes';
 
+import DBBuilder from '../../builder/DBBuilder.js';
+
 const router = Router();
 
 router.get('/build', (req, res) => {
-  sendResponse(res, { message: 'hi' }, StatusCodes.OK);
+  try {
+    const data = {
+      models: [
+        {
+          User: {
+            columns: [
+              {
+                name: 'd2username1',
+                type: 'char',
+
+                properties: {
+                  null: true,
+                  blank: true,
+                  default: 'ss',
+                  max_length: 22,
+                  choices: ['ss', 'Aa'],
+                  related_name: 'S'
+                }
+              },
+              {
+                name: 'car',
+                type: 'foreign_key',
+                foreign_key: {
+                  to: 'Car',
+                  on_delete: 'CASCADE'
+                }
+              },
+              {
+                name: 'car2',
+                type: 'one_to_one',
+                one_to_one: {
+                  to: 'Car',
+                  on_delete: 'CASCADE'
+                }
+              }, {
+                name: 'car3',
+                type: 'many_to_many',
+                many_to_many: {
+                  to: 'Car'
+                }
+              }
+            ],
+            timestambed: true,
+            isuser: true,
+            meta: {
+              db_table: 'my_user_table',
+              ordering: '-created',
+              verbose_name: 'asdasd',
+              verbose_name_plural: 'asdasd',
+              abstract: false
+            }
+          }
+        },
+        {
+          Car: {
+            columns: [
+              {
+                name: 'model',
+                type: 'char'
+              }
+            ]
+          }
+        }
+      ]
+    };
+    for (const d of DBBuilder.build(data)) {
+      console.log(d.code);
+    }
+
+    sendResponse(res, { result: DBBuilder.build(data) }, StatusCodes.OK);
+  } catch (error) {
+    sendResponse(res, { message: error.message }, StatusCodes.BAD_REQUEST);
+  }
 });
 
 export default router;
